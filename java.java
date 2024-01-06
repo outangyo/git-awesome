f5732b0e04eb432c5a5e733502bf42bc665f20ff
pongkorn <pongkorn.l169@gmail.com>
2024-01-06 16:31:58 +0700

    add some animals

--- java.java
+++ java.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main (String[] args){
+        System.out.println(Anarconda!!);
         System.out.println(buffalo!!);
+        System.out.println(Cow!!);
+        System.out.println(Dog!!);
+        System.out.println(Eagle!!);
+        System.out.println(fox!!);
+
     }
 }
